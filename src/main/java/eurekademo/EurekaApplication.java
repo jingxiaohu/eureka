7f96defc8d3fdba3eac4e3dc09d564d9d4872ab4
jingxiaohu <251878350@qq.com>
2017-11-15 13:56:43 +0800

    spring-cloud-eureka:server

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -1,9 +1,12 @@
 package eurekademo;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.stereotype.Component;
 
 /**
  *
@@ -12,10 +15,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCircuitBreaker
 public class EurekaApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
-
 }
+
+
